7ab5d4d4713ba5f5f0975d9c2100d0f90d1804df
Jefferson Dallalibera <jeffersonmigueldallalibera@gmail.com>
2022-01-26 11:27:52 -0300

    Ajuste de tela e criação aba chat

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,9 @@
 ﻿import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import appConfig from '../config.json';
+import React from 'react';
+import {useRouter} from 'next/router';
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,16 +35,17 @@ function Titulo(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'JeffersonDallalibera';
-
+  //const username = 'JeffersonDallalibera';
+  const [username, setUsername] = React.useState('JeffersonDallalibera')
+  const roteamento = useRouter();
+  
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+          //backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/07/super-mario-bros-level-ending.jpg)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
@@ -91,17 +67,49 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function(infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log("Alguem submeteu o  formulario")  
+             //trocar de pagina modo roots
+            //window.location.href ='/chat'
+            roteamento.push('chat');
+            
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Titulo tag="h2">Boas vindas de volta!</Titulo>
+            <Titulo tag="h2">Bem-vindo novamente</Titulo>
             <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
               {appConfig.name}
             </Text>
 
+
+            {/* <input
+             type="text"
+             value={username}
+             onChange={function Handler(event){
+               console.log("usuario digitou", event.target.value)
+               //Valores
+              const valor = event.target.value
+              //Trocar valor da variavel
+              //atraves do react
+              setUsername(valor);
+            }}
+            /> */}
+
+
             <TextField
+              value={username}
+              onChange={function Handler(event){
+              console.log("usuario digitou", event.target.value)
+              //Valores
+              const valor = event.target.value
+              //Trocar valor da variavel
+              //atraves do react
+              setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -111,7 +119,7 @@ export default function PaginaInicial() {
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
               }}
-            />
+            /> 
             <Button
               type='submit'
               label='Entrar'
